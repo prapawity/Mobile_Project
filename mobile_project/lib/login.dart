0e1d5ea608962ef67b9695fae0286df987a4fb25
boomNDS <boomsixboom@gmail.com>
2019-04-11 16:25:11 +0700

    fix that thing firebase

--- mobile_project/lib/login.dart
+++ mobile_project/lib/login.dart
@@ -3,7 +3,7 @@ import 'package:splashscreen/splashscreen.dart';
 import 'data.dart';
 import 'package:color/color.dart';
 import 'package:mobile_project/restaurant_list_screen.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 class Splash extends StatefulWidget {
   @override
   SplashState createState() => new SplashState();
@@ -23,12 +23,13 @@ class SplashState extends State<Splash> {
 }
 
 class AfterSplash extends StatelessWidget {
+  FirebaseAuth auth = FirebaseAuth.instance;
   final TextEditingController _controller = new TextEditingController();
   final TextEditingController _controller2 = new TextEditingController();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   bool state = false, chk2 = false;
-  String user, pass;
   final _formKey = GlobalKey<FormState>();
+  String user = "", pass = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,22 +92,41 @@ class AfterSplash extends StatelessWidget {
                   Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 15)),
                   RaisedButton(
                     child: Text("SignIn"),
-                    onPressed: () {
+                    onPressed: () async{
                       bool chk = false;
-                      _formKey.currentState.validate();
-                      for (var item in UserPass.idPass) {
-                        if (user == item[0] && pass == item[1]) {
-                          Navigator.of(context)
-                          .pushNamedAndRemoveUntil('/daily', (Route<dynamic> route) => false);
-                          chk = true;
-                        }
+                     
+                      // auth.createUserWithEmailAndPassword(
+                      //   email: user,
+                      //   password: pass
+                      // );
+                      if( _formKey.currentState.validate()){
+                        print("user: $user");
+                        print("pass: $pass");
+                        await auth.signInWithEmailAndPassword(
+                          email: this.user,
+                          password: this.pass
+                        ).then((FirebaseUser userfire){
+                          print(userfire);
+                        });
                       }
-                      if (chk == false) {
-                        _displaySnackBar(context);
-                      }
-                      _controller.clear();
-                      _controller2.clear();
-                      chk2 = false;
+
+
+
+
+
+                      // for (var item in UserPass.idPass) {
+                      //   if (user == item[0] && pass == item[1]) {
+                      //     Navigator.of(context)
+                      //     .pushNamedAndRemoveUntil('/daily', (Route<dynamic> route) => false);
+                      //     chk = true;
+                      //   }
+                      // }
+                      // if (chk == false) {
+                      //   _displaySnackBar(context);
+                      // }
+                      // _controller.clear();
+                      // _controller2.clear();
+                      // chk2 = false;
                     },
                     color: Colors.blue,
                     splashColor: Colors.blueGrey,
@@ -115,10 +135,18 @@ class AfterSplash extends StatelessWidget {
                   RaisedButton(
                     child: Text("SignUp"),
                     onPressed: () {
+                      print("signup $user $pass");
+                      auth.createUserWithEmailAndPassword(
+                        email: user,
+                        password: pass,
+                      ).then((FirebaseUser userid){
+                        Navigator.pushNamed(context, "/information");
+                      }
+                      );
                       // bool chk = false;
                       // _formKey.currentState.validate();
                       // UserPass.idPass.add([user,pass]);
-                      Navigator.pushNamed(context, "/information");
+                      // Navigator.pushNamed(context, "/information");
                       // chk = true;
 
                       // if (chk == false) {
