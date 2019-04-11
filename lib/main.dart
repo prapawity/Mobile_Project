6f8371da670ab4298cf4c319f3380b804fecabc5
boomNDS <boomsixboom@gmail.com>
2019-04-12 00:57:39 +0700

    :wrench: fix path in project & set icon for app

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_project/ui/login.dart';
+import 'package:mobile_project/ui/informationForm.dart';
+import 'package:mobile_project/ui/dailyMain.dart';
+import 'package:mobile_project/ui/restaurant_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.orange,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) =>Splash(),
+        "/information": (context) =>InfromationForm(),
+        "/ResraurantListScreen": (context) =>ResraurantListScreen(),
+         "/daily":  (context) =>dailyMain(),
+        // "/third": (context) =>(SignUp()),
+
+        },
+    );
+  }
+}
