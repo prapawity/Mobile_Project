50a9c17011c9c78fbc8f6d2abbee4cf47259d714
boomNDS <boomsixboom@gmail.com>
2019-05-18 01:49:02 +0700

    circle firebase

--- lib/ui/dailyMain.dart
+++ lib/ui/dailyMain.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -9,6 +10,7 @@ import 'package:mobile_project/ui/informationForm.dart';
 import 'package:mobile_project/ui/login.dart';
 import 'package:mobile_project/ui/customMenu.dart';
 import 'package:mobile_project/ui/getdata.dart';
+import 'package:mobile_project/service/userinfo.dart';
 
 class dailyMain extends StatefulWidget {
   const dailyMain({Key key, this.user}) : super(key: key);
@@ -17,6 +19,8 @@ class dailyMain extends StatefulWidget {
   dailyMainState createState() => dailyMainState();
 }
 
+Color labelColor = Colors.blue[200];
+
 class dailyMainState extends State<dailyMain> {
   String imgprofile =
       "https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.0-9/40432184_1828845493817855_5173684245750611968_o.jpg?_nc_cat=101&_nc_ht=scontent.fbkk5-6.fna&oh=aa1b368524a1ae602cd8d6a5e159fd91&oe=5D38E56E";
@@ -24,9 +28,7 @@ class dailyMainState extends State<dailyMain> {
   final GlobalKey<AnimatedCircularChartState> _chartKey =
       new GlobalKey<AnimatedCircularChartState>();
   final _chartSize = const Size(200.0, 200.0);
-  double value = 50.0;
-  Color labelColor = Colors.blue[200];
-
+  double value = 0;
 // Tab setup
   final Map<int, Widget> pagelist = const <int, Widget>{
     0: Text("อาหาร"),
@@ -54,212 +56,204 @@ class dailyMainState extends State<dailyMain> {
     ),
   };
   int sharedValue = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+  Widget buildUi(BuildContext context, userinfo nowuser) {
+    value = (nowuser.calnow / (nowuser.calmax / 100)).toDouble();
+    List<CircularStackEntry> data = _generateChartData(value);
+    _chartKey.currentState.updateData(data);
     TextStyle _labelStyle = Theme.of(context)
         .textTheme
         .title
         .merge(new TextStyle(color: labelColor));
+
     return Scaffold(
-        appBar: new AppBar(
-          title: new Text("title ${widget.user.email}"),
-          // title: new Text("title ${widget.user.email}"),
-          backgroundColor: Colors.orange,
+      appBar: new AppBar(
+        title: new Text("title ${widget.user.email}"),
+        // title: new Text("title ${widget.user.email}"),
+        backgroundColor: Colors.orange,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          shape: const CircleBorder(),
-          onPressed: () {
-            Navigator.of(context).push(new MaterialPageRoute(
-                builder: (BuildContext context) => new Menu()));
-          },
-        ),
-        endDrawer: new Drawer(
-          child: new ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(
-                accountName: new Text('${widget.user.uid}'),
-                accountEmail: new Text('${widget.user.email}'),
-                currentAccountPicture: new GestureDetector(
-                  // InfromationForm
-                  onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          new InfromationForm(user: widget.user))),
-                  // onDoubleTap: () => print("profile click"),
-                  child: new CircleAvatar(
-                    backgroundImage: new NetworkImage(imgprofile),
-                  ),
-                ),
-                decoration: new BoxDecoration(
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new NetworkImage(
-                            "https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/running.jpg"))),
-              ),
-              new ListTile(
-                title: new Text("restaurant"),
-                trailing: new Icon(Icons.restaurant_menu),
+        shape: const CircleBorder(),
+        onPressed: () {
+          Navigator.of(context).push(new MaterialPageRoute(
+              builder: (BuildContext context) => new Menu()));
+        },
+      ),
+      endDrawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('${widget.user.uid}'),
+              accountEmail: new Text('${widget.user.email}'),
+              currentAccountPicture: new GestureDetector(
+                // InfromationForm
                 onTap: () => Navigator.of(context).push(new MaterialPageRoute(
                     builder: (BuildContext context) =>
-                        new ResraurantListScreen())),
+                        new InfromationForm(user: widget.user))),
+                // onDoubleTap: () => print("profile click"),
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage(imgprofile),
+                ),
               ),
-              new ListTile(
-                title: new Text("menulist"),
-                trailing: new Icon(Icons.list),
-                onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) => new Menu())),
-              ),
-              new ListTile(
-                title: new Text("Graph"),
-                trailing: new Icon(Icons.assessment),
-                onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) => new Menu())),
-              ),
-              new ListTile(
-                title: new Text("Add menu"),
-                trailing: new Icon(Icons.add),
-                onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) => new Add())),
-              ),
-              new ListTile(
-                title: new Text("get Data firebase"),
-                trailing: new Icon(Icons.add),
-                onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) => new getfirebase())),
-              ),
-              new Divider(),
-              new ListTile(
-                title: new Text("Sign out"),
-                trailing: new Icon(Icons.exit_to_app),
-                onTap: () => _signOut,
-              )
-            ],
-          ),
+              decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new NetworkImage(
+                          "https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/running.jpg"))),
+            ),
+            new ListTile(
+              title: new Text("restaurant"),
+              trailing: new Icon(Icons.restaurant_menu),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      new ResraurantListScreen())),
+            ),
+            new ListTile(
+              title: new Text("menulist"),
+              trailing: new Icon(Icons.list),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) => new Menu())),
+            ),
+            new ListTile(
+              title: new Text("Graph"),
+              trailing: new Icon(Icons.assessment),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) => new Menu())),
+            ),
+            new ListTile(
+              title: new Text("Add menu"),
+              trailing: new Icon(Icons.add),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) => new Add())),
+            ),
+            new ListTile(
+              title: new Text("get Data firebase"),
+              trailing: new Icon(Icons.add),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) => new getfirebase())),
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("Sign out"),
+              trailing: new Icon(Icons.exit_to_app),
+              onTap: () => print("a"),
+            )
+          ],
         ),
-        body: new Container(
-          padding: EdgeInsets.all(30.0),
-          child: new Column(children: <Widget>[
-            FlatButton(child: new AnimatedCircularChart(
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(30.0),
+        child: new Column(children: <Widget>[
+          FlatButton(
+            child: new AnimatedCircularChart(
               key: _chartKey,
               size: _chartSize,
               initialChartData: _generateChartData(value),
               chartType: CircularChartType.Radial,
               edgeStyle: SegmentEdgeStyle.round,
               percentageValues: true,
-              holeLabel: '$value cal',
+              holeLabel: '${nowuser.calnow} cal',
               labelStyle: _labelStyle,
             ),
-            onPressed: (){
+            onPressed: () {
               print("clicked");
-            },),
-            new SizedBox(
-              height: 20,
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                    width: 300.0,
-                    child: CupertinoSegmentedControl<int>(
-                      children: pagelist,
-                      onValueChanged: (int val) {
-                        setState(() {
-                          sharedValue = val;
-                        });
-                      },
-                      groupValue: sharedValue,
-                    ))
-              ],
-            ),
-            Expanded(
-              child: Padding(
+            },
+          ),
+          new SizedBox(
+            height: 20,
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                  width: 300.0,
+                  child: CupertinoSegmentedControl<int>(
+                    children: pagelist,
+                    onValueChanged: (int val) {
+                      setState(() {
+                        sharedValue = val;
+                      });
+                    },
+                    groupValue: sharedValue,
+                  ))
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 32.0,
+                horizontal: 16.0,
+              ),
+              child: Container(
                 padding: const EdgeInsets.symmetric(
-                  vertical: 32.0,
+                  vertical: 64.0,
                   horizontal: 16.0,
                 ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 64.0,
-                    horizontal: 16.0,
-                  ),
-                  child: icons[sharedValue],
-                ),
+                child: icons[sharedValue],
               ),
             ),
-          ]),
-        ));
-  }
-
-  void _increment() {
-    setState(() {
-      if (value < 100) {
-        value += 10;
-      }
-      List<CircularStackEntry> data = _generateChartData(value);
-      _chartKey.currentState.updateData(data);
-    });
-  }
-
-  void _decrement() {
-    setState(() {
-      if (value > 0) {
-        value -= 10;
-      }
-      List<CircularStackEntry> data = _generateChartData(value);
-      _chartKey.currentState.updateData(data);
-    });
-  }
-
-  List<CircularStackEntry> _generateChartData(double value) {
-    Color dialColor = Colors.blue[200];
-    if (value < 0) {
-      dialColor = Colors.red[200];
-    } else if (value < 50) {
-      dialColor = Colors.red[200];
-    }
-    labelColor = dialColor;
-
-    List<CircularStackEntry> data = <CircularStackEntry>[
-      new CircularStackEntry(
-        <CircularSegmentEntry>[
-          new CircularSegmentEntry(
-            value,
-            dialColor,
-            rankKey: 'percentage',
-          )
-        ],
-        rankKey: 'percentage',
+          ),
+        ]),
       ),
-    ];
-
-    // if (value > 100) {
-    //   labelColor = Colors.green[200];
-
-    //   data.add(new CircularStackEntry(
-    //     <CircularSegmentEntry>[
-    //       new CircularSegmentEntry(
-    //         value - 100,
-    //         Colors.green[200],
-    //         rankKey: 'percentage',
-    //       ),
-    //     ],
-    //     rankKey: 'percentage2',
-    //   ));
-    // }
-
-    return data;
+    );
   }
 
-  void _signOut() {
-    FirebaseAuth.instance.signOut();
-    print("signout");
-    Navigator.of(context).push(
-        new MaterialPageRoute(builder: (BuildContext context) => new Splash()));
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+      stream: Firestore.instance
+          .collection('users')
+          .document('${widget.user.email}')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        final nowuser = userinfo.fromSnapshot(snapshot.data);
+        return buildUi(context, nowuser);
+      },
+    );
   }
+}
+
+List<CircularStackEntry> _generateChartData(double value) {
+  Color dialColor = Colors.blue[200];
+  if (value < 0) {
+    dialColor = Colors.red[200];
+  } else if (value < 50) {
+    dialColor = Colors.red[200];
+  }
+  labelColor = dialColor;
+
+  List<CircularStackEntry> data = <CircularStackEntry>[
+    new CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(
+          value,
+          dialColor,
+          rankKey: 'percentage',
+        )
+      ],
+      rankKey: 'percentage',
+    ),
+  ];
+
+  if (value > 100) {
+    labelColor = Colors.green[200];
+
+    data.add(new CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(
+          value - 100,
+          Colors.green[200],
+          rankKey: 'percentage',
+        ),
+      ],
+      rankKey: 'percentage2',
+    ));
+  }
+
+  return data;
 }
