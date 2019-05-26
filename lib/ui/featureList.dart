bcd6b0844dc61649152d819dc28c90e519b34630
wewarwo <32860454+wewarwo@users.noreply.github.com>
2019-05-26 21:38:09 +0700

    futurelist

--- lib/ui/featureList.dart
+++ lib/ui/featureList.dart
@@ -1,42 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:intro_slider/intro_slider.dart';
 
-class FeatureList extends StatefulWidget{
+class FeatureList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return FeatureListState();
   }
-
 }
-class FeatureListState extends State<FeatureList>{
-   List<Slide> slides = new List();
+
+class FeatureListState extends State<FeatureList> {
+  List<Slide> slides = new List();
 
   @override
   void initState() {
     super.initState();
+
     slides.add(
       new Slide(
         title: "กรุณายืนยัน E-mail ของท่าน",
-        description: "คุณจะต้องทำการยืนยันอีเมลที่เราส่งไปในอีเมลของทคุณก่อนการใช้งาน LifeBoost",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "คุณจะต้องทำการยืนยันอีเมลที่เราส่งไปในอีเมลของทคุณก่อนการใช้งาน LifeBoost",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
         pathImage: "resource/email.png",
         backgroundColor: Color(0xff406E8E),
-        
       ),
     );
     slides.add(
       new Slide(
         title: "การคำนวณแคลอรี่",
-        description: "เราจะทำการเก็บแคลอรี่ในแต่ละวัน\nที่มาจากการรับประทานอาหารและออกกำลังกายของคุณ",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "เราจะทำการเก็บแคลอรี่ในแต่ละวัน\nที่มาจากการรับประทานอาหารและออกกำลังกายของคุณ",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
         pathImage: "resource/calculator.png",
         backgroundColor: Color(0xffD78521),
-        
       ),
     );
     slides.add(
       new Slide(
         title: "การออกกำลังกาย",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
         description: "เราจะแนะนำการออกกำลังกายที่จะช่วยเผาผลาญแคลอรี่ของคุณได้",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
         pathImage: "resource/exercise.png",
         backgroundColor: Color(0xff203152),
       ),
@@ -44,17 +75,35 @@ class FeatureListState extends State<FeatureList>{
     slides.add(
       new Slide(
         title: "ร้านอาหาร",
-        description:
-        "เราจะแนะนำร้านอาหารที่อยู่ใกล้คุณ เพื่อที่คุณจะสามารถทราบที่อยู่และรายละเอียดของร้านอาหารที่สนใจได้",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description: "เราจะแนะนำร้านอาหารที่อยู่ใกล้คุณ เพื่อที่คุณจะสามารถทราบที่อยู่และรายละเอียดของร้านอาหารที่สนใจได้",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
         pathImage: "resource/res.png",
         backgroundColor: Color(0xff9932CC),
       ),
     );
-    slides.add(
+        slides.add(
       new Slide(
         title: "แผนที่ร้านอาหาร",
-        description:
-        "เราจะแสดงแผนที่ของร้านอาหารที่คุณสนใจ",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description: "เราจะแสดงแผนที่ของร้านอาหารที่คุณสนใจ",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
         pathImage: "resource/map.png",
         backgroundColor: Color(0xff90323D),
       ),
@@ -63,15 +112,111 @@ class FeatureListState extends State<FeatureList>{
 
   void onDonePress() {
     // Do what you want
-    Navigator.pop(context);
   }
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new IntroSlider(
-      slides: this.slides,
-      onDonePress: this.onDonePress,
+
+  void onTabChangeCompleted(index) {
+    // Index of current tab is focused
+  }
+
+  Widget renderNextBtn() {
+    return Icon(
+      Icons.navigate_next,
+      color: Color(0xffffcc5c),
+      size: 35.0,
     );
   }
 
+  Widget renderDoneBtn() {
+    return Icon(
+      Icons.done,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  Widget renderSkipBtn() {
+    return Icon(
+      Icons.skip_next,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  List<Widget> renderListCustomTabs() {
+    List<Widget> tabs = new List();
+    for (int i = 0; i < slides.length; i++) {
+      Slide currentSlide = slides[i];
+      tabs.add(Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 60.0, top: 60.0),
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                  child: Image.asset(
+                currentSlide.pathImage,
+                width: 200.0,
+                height: 200.0,
+                fit: BoxFit.contain,
+              )),
+              Container(
+                child: Text(
+                  currentSlide.title,
+                  style: currentSlide.styleTitle,
+                  textAlign: TextAlign.center,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+              Container(
+                child: Text(
+                  currentSlide.description,
+                  style: currentSlide.styleDescription,
+                  textAlign: TextAlign.center,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+    return tabs;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new IntroSlider(
+      // List slides
+      slides: this.slides,
+
+      // Skip button
+      renderSkipBtn: this.renderSkipBtn(),
+      colorSkipBtn: Color(0x33ffcc5c),
+      highlightColorSkipBtn: Color(0xffffcc5c),
+
+      // Next button
+      renderNextBtn: this.renderNextBtn(),
+
+      // Done button
+      renderDoneBtn: this.renderDoneBtn(),
+      onDonePress: this.onDonePress,
+      colorDoneBtn: Color(0x33ffcc5c),
+      highlightColorDoneBtn: Color(0xffffcc5c),
+
+      // Dot indicator
+      colorDot: Color(0x33ffcc5c),
+      colorActiveDot: Color(0xffffcc5c),
+      sizeDot: 13.0,
+
+      // List custom tabs
+      listCustomTabs: this.renderListCustomTabs(),
+
+      // Show or hide status bar
+      shouldHideStatusBar: true,
+
+      // On tab change completed
+      onTabChangeCompleted: this.onTabChangeCompleted,
+    );
+  }
 }
